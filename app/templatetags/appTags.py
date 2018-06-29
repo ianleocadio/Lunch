f5f1cf3288cf7c35e6a12865b002b2104c97e70f
Ian Leocádio <ian.leocadio@techne.com.br>
2018-06-29 16:04:32 -0300

    add

--- app/templatetags/appTags.py
+++ app/templatetags/appTags.py
@@ -3,13 +3,38 @@ from django.template.loader import get_template
 import datetime, calendar
 from app.models import Month
 
+
 register = template.Library()
 
-@register.inclusion_tag(filename=get_template('daysInMonth.html'), takes_context=True, name="calendar")
+
+@register.inclusion_tag(get_template('app/daysInMonth.html'), takes_context=True)
 def display_calendar(context, month:Month, year):
-    #month = context[""]
     #weekDisplayInMonth = calendar.monthcalendar(month=month.month, year=year)
-    #print(weekDisplayInMonth)
+    context["weekDisplayInMonth"] = month.day_set.all()
+    #seg, ter, qua, qui, sex, sab, dom
+
+    return context
+
+
+@register.inclusion_tag(get_template('app/days.html'), takes_context=True)
+def display_days(context, month:Month, year):
+
+    weekDisplayInMonth = calendar.monthcalendar(month=month.month, year=year)
+    days = month.day_set.all()
+    lis = []
+    for i,week in enumerate(weekDisplayInMonth):
+        lis.append([])
+        for d in week:
+            if d != 0:
+                d = days.get(day=d)
+            lis[i].append(d)
+
+
+
+    context["weeksOnMonth"] = lis
+    context["weekSiglas"] = ["M", "T", "W", "T", "F", "S", "S"]
+
+    #seg, ter, qua, qui, sex, sab, dom
 
     return context
 
